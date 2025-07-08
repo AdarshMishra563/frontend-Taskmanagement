53291403e9ed20c33ef170b0596b2f4ceaf426cf
AdarshMishra563 <am0070563@gmail.com>
2025-07-09 01:08:36 +0530

    new

--- src/app/component/Fixedviewchild.js
+++ src/app/component/Fixedviewchild.js
@@ -4,8 +4,8 @@ const Modal = ({ isOpen, onClose, children }) => {
   if (!isOpen) return null;
 
   return (
-    <div  style={{ backgroundColor: 'rgba(0, 0, 0, 0.4)' }}  className="fixed inset-0   flex items-center justify-center z-50 pt-4">
-      <div  className="bg-gray-800 p-6 rounded-lg shadow-lg w-[90%] max-w-lg relative">
+    <div  style={{ backgroundColor: 'rgba(0, 0, 0, 0.4)' }}  className="fixed inset-0   flex items-center justify-center z-50 pt-6">
+      <div  className="bg-gray-800 p-4 rounded-lg shadow-lg w-[90%] max-w-lg relative">
         <button
           onClick={onClose}
           className="absolute top-2 right-3 text-gray-400 hover:text-white text-3xl"
